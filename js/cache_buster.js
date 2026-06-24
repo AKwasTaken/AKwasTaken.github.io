b27f6bc9dcee1a4044ee6b4c1bffd2d2cedb8719
Aneeth Kumaar <113939717+AKwasTaken@users.noreply.github.com>
2026-06-24 17:25:01 +0530

    oops mistake correction

--- js/cache_buster.js
+++ js/cache_buster.js
@@ -3,6 +3,6 @@ link.rel = "stylesheet";
 
 // Appends a unique timestamp down to the minute so it stays fresh while editing
 const timestamp = Math.floor(Date.now() / 60000);
-link.href = "css/style.css?v=" + timestamp;
+link.href = "/css/style.css?v=" + timestamp;
 
 document.head.appendChild(link);
